3fac3a9bdb7dda28a0ee9495ee2d7714a9b8b55d
shun0750 <shunkun0750@gmail.com>
2020-03-15 13:14:48 +0900

    adding gstelements

--- 360-degree-dewarper/main.c
+++ 360-degree-dewarper/main.c
@@ -11,10 +11,20 @@
 #include "gst-nvmessage.h"
 #endif
 
-int main (int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-    g_printerr ("Initializeing app...\n");
+    GMainLoop *loop = NULL;
+    GstElement *pipeline = NULL, *source = NULL, *h264parser = NULL
+
+    /* GStreamer initialization */
+    g_print ("Initializeing app...\n");
+    loop = g_main_loop_new (NULL, FALSE);
     gst_init (&argc, &argv);
+
+    /* -- 1.Create GStreamer Objects -- */
+    /* Create pipeline */
+    pipeline = gst_pipeline_new ("pipeline");
 
     GstElement *source;
     source = gst_element_factory_make ("autovideosrc", "source");
